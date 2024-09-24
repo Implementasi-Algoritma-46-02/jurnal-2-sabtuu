73c4a5aa36fc77f5176f610c5422749eb4ab3285
sabtu <sabilgaming26@gmail.com>
2024-09-24 16:56:12 +0700

    sabut

--- Jurnal1.java
+++ Jurnal1.java
@@ -2,5 +2,7 @@ public class Jurnal1 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        System.out.println("hello");
+        System.out.println("world");
     }
 }
