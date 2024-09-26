0fc1f6d644cc8a828521fe8b4385624733042220
sabtuu <sabilgaming26@gmail.com>
2024-09-26 15:28:17 +0700

    ngoding

--- Jurnal1.java
+++ Jurnal1.java
@@ -2,7 +2,7 @@ public class Jurnal1 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
-        System.out.println("hello");
-        System.out.println("world");
+        System.out.println("Hello World!!");
+    
     }
 }
